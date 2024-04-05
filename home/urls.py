3f4a953f3037c7659281d45408fac157b94bac4b
DiarTor <mhmmd2020.15@gmail.com>
2024-04-05 14:05:36 +0330

    added basic html to homepage

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from home.views import home
+from home import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home-page'),
+    path('posts', views.posts, name='posts-page'),
+    path('posts/<slug:slug>', views.single_post, name='post-detail-page')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world. You're at the home index.")
+    return render(request, 'home/homepage.html')
+
+
+def posts(request):
+    pass
+
+
+def single_post(request):
+    pass
