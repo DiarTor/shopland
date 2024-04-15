574c3071ab2c2903e3d28cb5af1c54d73ce69e29
DiarTor <mhmmd2020.15@gmail.com>
2024-04-15 11:30:49 +0330

    restarted the project

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.home, name='home-page'),
-    path('posts', views.posts, name='posts-page'),
-    path('posts/<slug:slug>', views.single_post, name='post-detail-page')
-]
