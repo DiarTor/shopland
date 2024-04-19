31929fbdc97919c3f2461846ddfd2b390bd77969
DiarTor <mhmmd2020.15@gmail.com>
2024-04-19 10:33:34 +0330

    added slug + admin

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from product_module.models import Product
+
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields = ['slug']
+    prepopulated_fields = {'slug': ['title']}
+    list_display = ['title', 'price', 'short_description', 'rating', 'is_active']
+    list_filter = ['is_active', 'rating']
+    list_editable = ['is_active', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
--- product_module/views.py
+++ product_module/views.py
@@ -1,19 +1,22 @@
-from django.http import Http404
-from django.shortcuts import render,get_object_or_404
+from django.db.models import Avg
+from django.shortcuts import render, get_object_or_404
 
 from product_module.models import Product
 
 
 # Create your views here.
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'product_module/product_list.html', {"products": products})
+    products = Product.objects.all().order_by('title')
+    total_products = products.count()
+    avg_rating = products.aggregate(Avg('rating'))
+    return render(request, 'product_module/product_list.html',
+                  {"products": products, "total_products": total_products, "avg_rating": avg_rating})
 
 
-def product_detail(request, product_id):
+def product_detail(request, slug):
     # try:
     #     product = Product.objects.get(id=product_id)
     # except:
     #     raise Http404("Product does not exist")
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, slug=slug)
     return render(request, 'product_module/product_detail.html', {"product": product})
