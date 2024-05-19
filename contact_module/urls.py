e5e924ab3119de27852d56331fb146807951d56e
DiarTor <mhmmd2020.15@gmail.com>
2024-05-19 07:37:17 +0330

    Edited contact us form

--- contact_module/urls.py
+++ contact_module/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from contact_module.views import contact_us_page
 urlpatterns = [
-    path('', contact_us_page)
+    path('', contact_us_page, name="contact_us_page")
 ]
