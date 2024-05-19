478ded700313db1276f2da30ece7554bb0d01e6e
DiarTor <mhmmd2020.15@gmail.com>
2024-05-19 07:58:57 +0330

    created forms.py + created ContactUsForm

---
+++ contact_module/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ContactUsForm(forms.Form):
+    fullname = forms.CharField()
+    email = forms.EmailField()
+    subject = forms.CharField()
+    text = forms.CharField()
