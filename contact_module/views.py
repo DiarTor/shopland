b7efe1ed1c9b2865c2b3d3a30f4dc390de0e385f
DiarTor <mhmmd2020.15@gmail.com>
2024-05-19 08:37:06 +0330

    Upgraded ContactUsForm + added style and error message

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
-
-from contact_module.forms import ContactUsForm
-
+from .forms import ContactUsForm
 
 # Create your views here.
+from django.urls import reverse
+
 
 def contact_us_page(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         contact_form = ContactUsForm(request.POST)
         if contact_form.is_valid():
-            print(contact_form.changed_data)
-            return redirect(reverse("home_page"))
+            print(contact_form.cleaned_data)
+            return redirect('home_page')
     else:
         contact_form = ContactUsForm()
-    return render(request, 'contact_module/contact_us_page.html', {"contact_form": contact_form})
+
+    return render(request, 'contact_module/contact_us_page.html', {
+        'contact_form': contact_form
+    })
