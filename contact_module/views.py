c74c9140d8c834af4f9089f9c5155f7097342c90
DiarTor <mhmmd2020.15@gmail.com>
2024-05-23 23:00:01 +0330

    clean code

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,8 +1,7 @@
 from django.views.generic import ListView
-
-from .forms import ContactUsModelForm
 from django.views.generic.edit import CreateView
 
+from .forms import ContactUsModelForm
 from .models import UserProfile
 
 
@@ -10,12 +9,6 @@ class ContactUsView(CreateView):
     form_class = ContactUsModelForm
     template_name = 'contact_module/contact_us_page.html'
     success_url = '/contact-us/'
-
-
-def store_file(file):
-    with open('temp/image.jpg', "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
 
 
 class CreateProfileView(CreateView):
